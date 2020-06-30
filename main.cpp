d602846a37131f8f0a278dc632eb0ffc49c1b828
root <vince@debian.local>
2020-06-30 15:41:45 +0200

    more comments on impl

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 
 using namespace CoGaDB;
 
+// Column Typ muss einfach mit meiner Impl ausgetauscht werden -> beim ersten zb run_length_compressed_column
+// beim 3. zb dictionary_compressed_column
 int main(){
 	/*Adapt the Column to your implemented method*/
             std::cout <<"Dic: "<< std::endl;
